f6b7679aa28655f4dde1a242bc7a0ff315934869
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2023-10-10 17:36:32 +0100

    gpt test

--- main.ts
+++ main.ts
@@ -1,13 +1,35 @@
-import { Plugin } from 'obsidian';
+import { Plugin, MarkdownView } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
     async onload() {
-        console.log('loading smart-tasklists');
-        // Code
+        this.addCommand({
+            id: 'access-codemirror-instance',
+            name: 'Access CodeMirror Instance',
+            callback: this.accessCodeMirror.bind(this),
+        });
     }
 
-    onunload() {
-        console.log('unloading smart-tasklists');
-        // Clean up, if necessary.
+    accessCodeMirror() {
+        const mkdview = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (!mkdview) {
+            console.log('No active Markdown view found.');
+            return;
+        }
+
+        const editor = mkdview.editor;
+        if (!editor) {
+            console.log('No editor found for the active view.');
+            return;
+        }
+
+        const cm = (editor as any).cm;
+        if (cm) {
+            console.log('Successfully accessed the CodeMirror instance.');
+            // You can now use the cm variable to interact with CodeMirror directly.
+        } else {
+            console.log('Failed to access the CodeMirror instance.');
+        }
     }
+
+    // ... rest of your plugin code ...
 }
