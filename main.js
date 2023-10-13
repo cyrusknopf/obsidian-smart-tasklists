189f1b2334171114f9b446fe57a755ce6d8bfdc5
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2023-10-13 18:29:03 +0100

    parent chain

--- main.js
+++ main.js
@@ -10,37 +10,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const obsidian_1 = require("obsidian");
-function checkLine(editor, pattern, lineNumber, parent) {
+function checkLine(editor, pattern, lineNumber, parrent_chain) {
     if (pattern.test(editor.getLine(lineNumber))) {
         console.log("There is a task at line: " + lineNumber);
         console.log("This task reads: " + editor.getLine(lineNumber));
-        checkLine(editor, pattern, lineNumber + 1, lineNumber);
+        console.log("It's parent is line" + parrent_chain[parrent_chain.length - 1]);
+        checkLine(editor, pattern, lineNumber + 1, parrent_chain);
     }
 }
 class ExamplePlugin extends obsidian_1.Plugin {
     onload() {
         return __awaiter(this, void 0, void 0, function* () {
             this.addCommand({
-                id: "insert-todays-date",
-                name: "Insert today's date",
-                editorCallback: (editor) => {
-                    const cursor = editor.getCursor();
-                    if (cursor)
-                        console.log("got cursor");
-                    const line = cursor.line;
-                },
-            });
-            this.addCommand({
                 id: "resize-pinned-tabs",
                 name: "Get tasks",
                 editorCallback: (editor) => {
                     const markdownView = this.app.workspace.getActiveViewOfType(obsidian_1.MarkdownView);
                     if (markdownView) {
-                        var task_lines = [];
                         const task_regex = /^\s+- \[ \]/;
                         for (var i = 0; i < editor.lastLine() + 1; i++) {
-                            if (editor.getLine(i).startsWith("- [ ]"))
-                                checkLine(editor, task_regex, i + 1, i);
+                            if (editor.getLine(i).startsWith("- [ ]")) {
+                                var parent_chain = [i];
+                                checkLine(editor, task_regex, i + 1, parent_chain);
+                            }
                         }
                     }
                 }
--- main.ts
+++ main.ts
@@ -1,44 +1,38 @@
 import { Editor, Plugin, MarkdownView } from 'obsidian';
 
-function checkLine(editor: Editor, pattern: RegExp, lineNumber: number, parent: number ) {
+function checkLine(editor: Editor, pattern: RegExp, lineNumber: number, parrent_chain: Array<number> ) {
     if (pattern.test(editor.getLine(lineNumber))) {
+
         console.log("There is a task at line: " + lineNumber);
         console.log("This task reads: " + editor.getLine(lineNumber));
-        checkLine(editor, pattern, lineNumber + 1, lineNumber)
+        console.log("It's parent is line" + parrent_chain[parrent_chain.length - 1])
+        checkLine(editor, pattern, lineNumber + 1, parrent_chain)
     }
 
 }
 
 
 export default class ExamplePlugin extends Plugin {
-    async onload() {
-      this.addCommand({
-        id: "insert-todays-date",
-        name: "Insert today's date",
-        editorCallback: (editor: Editor) => {
-            const cursor = editor.getCursor()
-            if (cursor) console.log("got cursor");
-            const line = cursor.line;
-        },
-      });
-    
+    async onload() {    
       this.addCommand({
         id: "resize-pinned-tabs",
         name: "Get tasks",
+        
         editorCallback: (editor: Editor) => {
             const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
             if (markdownView) {
-                var task_lines = [];
                 const task_regex = /^\s+- \[ \]/;
                 for (var i=0; i<editor.lastLine()+1; i++) {
-                    if (editor.getLine(i).startsWith("- [ ]")) checkLine(editor, task_regex, i +1 , i);
+                    if (editor.getLine(i).startsWith("- [ ]")) {
+                        var parent_chain: number[] = [i];
+                        checkLine(editor, task_regex, i +1, parent_chain);
+                
+                    }
                 }
             }
         }
 
         });
-    
-    
     }
 
 
