b2e8463ed320a054024501102cdec414695eafea
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2023-10-17 13:34:50 +0100

    Working recursive algorithm

--- main.js
+++ main.js
@@ -10,37 +10,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const obsidian_1 = require("obsidian");
-function checkLineForTasks(editor, pattern, lineNumber, parent_chain, indent) {
-    var line = editor.getLine(lineNumber);
-    if (!line)
-        return 0;
-    if (!pattern.test(line))
-        return 0;
-    const matches = line.match(pattern);
-    if (matches) {
-        // console.log("Indent: "+indent);
-        // console.log("matches length: " + matches[0].length);
-        if (matches[0].length > indent) {
-            indent = matches[0].length;
-            parent_chain.push(lineNumber - 1);
-            // console.log  ("There is a task at line: " + lineNumber);
-            // console.log("This task reads: " + editor.getLine(lineNumber));
-            // console.log("It's parent is line" + parent_chain[parent_chain.length - 1]);
-            checkLineForTasks(editor, pattern, lineNumber + 1, parent_chain, indent);
-        }
-        else if (matches[0].length == indent) {
-            parent_chain.push(parent_chain[parent_chain.length - 1]);
-            // console.log("There is a task at line: " + lineNumber);
-            // console.log("This task reads: " + editor.getLine(lineNumber));
-            // console.log("It's parent is line" + parent_chain[parent_chain.length - 1]);
-            checkLineForTasks(editor, pattern, lineNumber + 1, parent_chain, indent);
-            // console.log(parent_chain);
-        }
-        else {
-            return 0;
-        }
-    }
-}
 class Task {
     constructor(line, parent, indent, isDone, children) {
         this.line = line;
@@ -54,11 +23,14 @@ class Task {
         while (editor.getLine(this.line + i).match(task_regex)) {
             var match = editor.getLine(this.line + i).match(task_regex);
             if (match) {
-                if (match[0].length < this.indent) {
+                if (match[0].length > this.indent) {
                     var isDone = match[1] == "x";
                     var child = new Task(this.line + i, this.line, match[0].length, isDone, []);
                     child.getChildren(editor, task_regex);
                     this.children.push(child);
+                }
+                else {
+                    break;
                 }
             }
             i = i + 1;
@@ -87,26 +59,6 @@ class ExamplePlugin extends obsidian_1.Plugin {
                             }
                         }
                         console.log(tasks);
-                    }
-                }
-            });
-            this.addCommand({
-                id: "resize-pinned-tabs",
-                name: "Get tasks",
-                editorCallback: (editor) => {
-                    const markdownView = this.app.workspace.getActiveViewOfType(obsidian_1.MarkdownView);
-                    if (markdownView) {
-                        const task_regex = /^\s*- \[( |x)\]/;
-                        for (var i = 0; i < editor.lastLine() + 1; i++) {
-                            var match = editor.getLine(i).match(task_regex);
-                            if (match) {
-                                var parent_chain = [];
-                                // console.log(match[0].length);
-                                // console.log("for line" + editor.getLine(i));
-                                checkLineForTasks(editor, task_regex, i + 1, parent_chain, match[0].length);
-                                console.log(parent_chain);
-                            }
-                        }
                     }
                 }
             });
